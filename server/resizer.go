2973fa8e39177e0c077f76987f04c8c10fda5383
Jwalanta Shrestha <jwalanta@gmail.com>
2019-01-23 18:57:32 -0800

    few more features on the client side

--- server/resizer.go
+++ server/resizer.go
@@ -14,10 +14,10 @@ func resizeImages(fileList []string, n int, total int, srcFolder string, targetF
 
 		srcPath := srcFolder + "/" + filepath.Base(fileList[i])
 		destPath := targetFolder + "/" + filepath.Base(fileList[i])
-		//cmd := exec.Command("vipsthumbnail", srcPath, "-s", "1500x1500", "-o", destPath)
+		cmd := exec.Command("vipsthumbnail", srcPath, "-t", "-s", "1500x1500", "-o", destPath)
 		//cmd := exec.Command("convert", srcPath, "-resize", "1024x", destPath)
 		//cmd := exec.Command("epeg", "-m 1500", srcPath, destPath)
-		cmd := exec.Command("convert", "-define", "jpeg:size=1500x1500", srcPath, "-auto-orient", "-thumbnail", "1500x1500>", destPath)
+		//cmd := exec.Command("convert", "-define", "jpeg:size=1500x1500", srcPath, "-auto-orient", "-thumbnail", "1500x1500>", destPath)
 		err := cmd.Run()
 
 		fmt.Printf("resizer %d: [%d]=%s -- %v\n", n, i, fileList[i], err)
--- web/app.js
+++ web/app.js
@@ -1,95 +1,96 @@
-(function () {
+var photos = [];
+var pos = 0;
+var buckets = [[],[],[],[],[],[],[],[],[],[]];
 
-    var photos = [];
-    var pos = 0;
-    var buckets = [[],[],[],[],[],[],[],[],[],[]]
+var mod = function (n, m) {
+    return ((n % m) + m) % m;
+}
 
-    var mod = function (n, m) {
-        return ((n % m) + m) % m;
-    }
+var getID = function (id) {
+    return document.getElementById(id);
+}
 
-    var getID = function (id) {
-        return document.getElementById(id);
-    }
-
-    var updateFooter = function () {
-        getID('info').innerHTML = photos[pos] + " [" + (pos + 1) + "/" + photos.length + "]";
-    }
+var updateFooter = function () {
+    getID('info').innerHTML = photos[pos] + " [" + (pos + 1) + "/" + photos.length + "]";
+}
 
 
-    var displayPhoto = function () {
-        getID("photo").src = "t/" + photos[pos];
+var displayPhoto = function () {
+    getID("photo").src = "t/" + photos[pos];
+    updateFooter();
+    updateBucketDisplay()
+}
+
+var displayHiResPhoto = function () {
+
+    var p = getID("photo")
+
+    // dont update if the existing photo is big enough
+    if (p.naturalHeight < 1000 && p.naturalWidth < 1000) {
+        p.src = "p/" + photos[pos];
         updateFooter();
-        updateBucketDisplay()
     }
 
-    var displayHiResPhoto = function () {
-        getID("photo").src = "p/" + photos[pos];
-        updateFooter();
-        
+}
+
+var toggleBucket = function (i) {
+    var index = buckets[i].indexOf(photos[pos]);
+
+    if (index > -1) {
+        buckets[i].splice(index, 1);
+    } else {
+        buckets[i].push(photos[pos]);
     }
 
-    var updateBucketDisplay = function (){
-        for (var i=0;i<10;i++){
-            var b = getID("bucket"+i)
-            if (buckets[i].indexOf(photos[pos]) > -1){
-                if (!b.classList.contains("selected"))
-                    b.classList.add("selected")
+    updateBucketDisplay()
+}
 
-            }
-            else{
-                b.classList.remove("selected")
+var updateBucketDisplay = function () {
+    for (var i = 0; i < 10; i++) {
+        var b = getID("bucket" + i)
+        if (buckets[i].indexOf(photos[pos]) > -1) {
+            if (!b.classList.contains("selected"))
+                b.classList.add("selected")
 
-            }
+        } else {
+            b.classList.remove("selected")
+
         }
     }
+}
 
 
-    var request = new XMLHttpRequest();
-    request.open('GET', '/photos.json', true);
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-            photos = JSON.parse(request.responseText);
-            displayHiResPhoto();
-        }
-    };
-    request.send();
-
-    document.onkeydown = function (e) {
-        if (e.keyCode == 37) { // left
-            pos = mod(pos - 1, photos.length);
-            displayPhoto();
-        } else if (e.keyCode == 39 || e.keyCode == 32) { // right
-            pos = (pos + 1) % photos.length;
-            displayPhoto();
-        } else if (e.keyCode >=48 && e.keyCode <=57){
-            var i = e.keyCode - 48;
-
-            var index = buckets[i].indexOf(photos[pos]);
-
-            if (index > -1){
-                buckets[i].splice(index,1);
-            }
-            else{
-                buckets[i].push(photos[pos]);
-            }
-
-            updateBucketDisplay()
-
-        }
-    };
-
-    document.onkeyup = function (e) {
-        displayHiResPhoto()
+var request = new XMLHttpRequest();
+request.open('GET', '/photos.json', true);
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        photos = JSON.parse(request.responseText);
+        displayHiResPhoto();
     }
+};
+request.send();
 
-    var bucketStr = "";
-    
-    for (var i=1; i<=10;i++){
-        bucketStr += "<span id='bucket"+(i%10)+"'>"+(i%10)+"</span>";
+document.onkeydown = function (e) {
+    if (e.keyCode == 37) { // left
+        pos = mod(pos - 1, photos.length);
+        displayPhoto();
+    } else if (e.keyCode == 39 || e.keyCode == 32) { // right
+        pos = (pos + 1) % photos.length;
+        displayPhoto();
+    } else if (e.keyCode >= 48 && e.keyCode <= 57) {
+        var i = e.keyCode - 48;
+        toggleBucket(i)
     }
-    getID("buckets").innerHTML = bucketStr;
+};
 
+document.onkeyup = function (e) {
+    displayHiResPhoto()
+}
 
+var bucketStr = "";
 
-}).call(this);
+for (var i = 1; i <= 10; i++) {
+    bucketStr += "<span class='bucket' id='bucket" + (i % 10) + "' onclick='toggleBucket("+(i%10)+")'>" + (i % 10) + "</span>";
+}
+getID("buckets").innerHTML = bucketStr;
+
